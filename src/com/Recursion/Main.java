ed834ef71d8c39580b0e264861ecceddaa868d29
Nitin <nsinghboss02@gmail.com>
2022-02-21 22:50:46 +0530

    Queue implementation.

--- src/com/Recursion/Main.java
+++ src/com/Recursion/Main.java
@@ -9,6 +9,7 @@ public class Main {
         By default it only removes the top element.
         For insertion and deletion we have push ans pop method.
         And peek element will show the top most element.
+        Stack is a class in java collection framework.
          */
         Stack <Integer> stack = new Stack<>();
         stack.push(12);
@@ -22,5 +23,22 @@ public class Main {
         System.out.println(stack.pop());
         System.out.println(stack.pop());
         System.out.println(stack.pop());
+
+        /*
+        Queue:
+        it implements the first in first out and last in last out data structure.
+        In queue you can insert the element from back and insert it from the front.
+        Queue is an interface in java collection framework.
+        So we can not directly make an object of it so for constructor we will use linked list.
+         */
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(121);
+        queue.add(122);
+        queue.add(123);
+        queue.add(124);
+        queue.add(125);
+        queue.remove();
+        System.out.println(queue);
+        System.out.println(queue.peek());
     }
 }
